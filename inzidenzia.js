d892cd36d6c59555a0b9dfc3a9bc883decb074c5
Samuel Teuber <samuel@samweb.org>
2021-03-30 22:41:58 +0200

    Alpha Version of plugin

---
+++ inzidenzia.js
@@ -0,0 +1,42 @@
+function replaceTextOnPage(from, to){
+	var nodes = getAllTextNodes();
+	for(var i = 0; i < nodes.length; i++){
+		for (var j = 0; j<from.length; j++) {
+			  nodes[i].nodeValue = nodes[i].nodeValue.replace(new RegExp(from[j], 'ig'), to);
+		}
+	}
+  
+	function getAllTextNodes(){
+	  var result = [];
+  
+	  (function scanSubTree(node){
+		if(node.childNodes.length) {
+		  for(var i = 0; i < node.childNodes.length; i++) { 
+			scanSubTree(node.childNodes[i]);
+		  }
+		}else if(node.nodeType == Node.TEXT_NODE) {
+		  result.push(node);
+		}
+	  })(document.getElementsByTagName("body")[0]);
+  
+	  return result;
+	}
+  
+	function quote(str){
+	  return (str+'').replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+	}
+}
+
+function doReplacement() {
+	console.log("Replacement")
+	replaceTextOnPage(["Unabhängig von der (Wochen)?(7[ -]Tage[ -])?Inzidenz",
+	"Unabhängig vom Inzidenzwert",
+	"Unabhängig von Inzidenzwerten", "Unabhängig von Inzidenz"],"Unabhängig vom gesunden Menschenverstand")
+}
+
+//window.setInterval(function() {doReplacement()}, 1000)
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(doReplacement);
+const config = { attributes: true, childList: true, subtree: true };
+// Start observing the target node for configured mutations
+observer.observe(document, config);
